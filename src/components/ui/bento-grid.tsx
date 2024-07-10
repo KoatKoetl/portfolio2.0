078e23608cfd3b81f5a784c0c2764c18b4e86078
KoatKoetl <stas20027@outlook.com>
2024-07-10 21:45:16 +0300

    Finish the About me section component

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -1,4 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import { BackgroundGradientAnimation } from '@/components/ui/Background-gradient-animation';
 import { cn } from '@/utils/cn';
+import { useState } from 'react';
+import { IoCopyOutline } from 'react-icons/io5';
+import { GlobeDemo } from './GridGlobe';
+import ShimmerButton from './ShimmerButton';
 
 export const BentoGridItem = ({
   className,
@@ -19,6 +26,14 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('stanislavsuschevici@gmail.com');
+    setCopied(true);
+    setTimeout(() => setCopied(false), 10000);
+  };
+
   return (
     <div
       className={cn(
@@ -30,12 +45,71 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
       }}
     >
-      <div className="group-hover/bento:translate-x-2 transition duration-200 p-4">
+      {id === 6 && (
+        <div className="absolute">
+          <BackgroundGradientAnimation />
+        </div>
+      )}
+      <div
+        className={`group-hover/bento:scale-101 transition duration-200 p-4 z-50 max-h-fit min-h-48 ${
+          id === 3 && 'flex items-center md:block'
+        } ${id === 6 && 'flex flex-col justify-center items-center'}`}
+      >
         <div>{img && <img src={img} alt={img} className={cn(imgClassName, 'rounded-lg')} />}</div>
-        <div>{spareImg && <img src={spareImg} alt={spareImg} className="absolute bottom-12 right-5" />}</div>
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">{title}</div>
+        <div>
+          {spareImg && <img src={spareImg} alt={spareImg} className={`absolute ${id === 4 && 'bottom-12 md:bottom-12 md:left-0'}`} />}
+        </div>
+        <div
+          className={`font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2 ${id === 2 && 'text-xl'} 
+          ${id === 3 && 'text-xl text-wrap max-w-24 sm:max-w-max md:text-lg mb-0 mt-0'} ${
+            id === 6 && 'text-xl md:text-3xl text-center md:mb-10'
+          }`}
+        >
+          {title}
+        </div>
+        {id === 6 && (
+          <ShimmerButton
+            title={copied ? 'E-mail copied' : 'Copy e-mail address'}
+            icon={<IoCopyOutline />}
+            position="left"
+            handleClick={handleCopy}
+          />
+        )}
+
+        {id === 2 && <GlobeDemo />}
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-4 w-fit absolute right-3 lg:right-2">
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-6">
+              {['HTMl', 'CSS', 'JavaScript', 'SCSS'].map((item, i) => (
+                <span
+                  key={i}
+                  className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                >
+                  {item}
+                </span>
+              ))}
+              <span className="lg:py-7 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
+            </div>
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              <span className="lg:py-7 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              {['React', 'TypeScript', 'Tailwind', 'Shadcn/UI'].map((item, i) => (
+                <span
+                  key={i}
+                  className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
+
         <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">{description}</div>
       </div>
     </div>
   );
 };
+//
