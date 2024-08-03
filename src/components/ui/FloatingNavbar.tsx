b9128d7b7067b91d26c5aa293e2afb7f10d14af3
KoatKoetl <stas20027@outlook.com>
2024-08-03 20:40:28 +0300

    Fix the navigation

--- src/components/ui/FloatingNavbar.tsx
+++ src/components/ui/FloatingNavbar.tsx
@@ -52,11 +52,11 @@ const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          'flex max-w-fit  fixed top-10 inset-x-0 mx-auto border rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] py-2 px-4 sm:px-10 sm:py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100',
+          'flex max-w-fit fixed top-3 sm:top-10 inset-x-0 mx-auto border rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] py-2 px-4 sm:px-10 sm:py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100',
           className
         )}
       >
-        <a href="/" className="px-2 py-1 text-neutral-600 dark:text-neutral-50 dark:hover:text-neutral-300 hover:text-neutral-500">
+        <a href="/" className="px-8 sm:px-2 py-1 text-neutral-600 dark:text-neutral-50 dark:hover:text-neutral-300 hover:text-neutral-500">
           <button type="button" className="flex justify-center items-center">
             <FaHome />
           </button>
@@ -66,11 +66,11 @@ const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500'
+              'relative dark:text-neutral-50 items-center space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 hidden sm:flex'
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className="block">{navItem.icon}</span>
+            <span className="block text-sm">{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
